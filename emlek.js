94b92e67841417cb9996178b81f643318a2a2e6b
Marcell <marcellszlavik2014@gmail.com>
2026-03-21 22:03:05 +0100

    Add files via upload

---
+++ emlek.js
@@ -0,0 +1,41 @@
+// emlek.js
+if (typeof databases === 'undefined') {
+    var databases = {};
+}
+
+// Csak a 'emlek' kulcsot adjuk hozzá
+databases['emlek'] = [
+        { 
+            name: "Emlék érmék", 
+            id: "eml", 
+            years: [
+                {y: "2002 Kossuth 100Ft", m: "1 000 000", img: "emlek/kossuth.jpg"},
+                {y: "2003 Deák Ferenc 20Ft", m: "985.000", img: "emlek/deak.jpg"},
+		{y: "2004 EU csatlakozás 50Ft", m: "985.000"},
+		{y: "2005 József Attila 10Ft", m: "12.000 (csak bliszterben)"},
+		{y: "2005 Gyermekmentő 50Ft", m: "1.985.000"},
+		{y: "2006 Vöröskereszt 50Ft", m: "2.000.000"},
+		{y: "2006 1956-os forradalom 50Ft", m: "2.000.000"},
+		{y: "2007 Római szerződés 50Ft", m: "1.990.000"},
+		{y: "2004 Uniós 50Ft", m: "1 000 000"},
+		{y: "2004 Uniós 50Ft", m: "1 000 000"},
+		{y: "2004 Uniós 50Ft", m: "1 000 000"},
+		{y: "2004 Uniós 50Ft", m: "1 000 000"},
+		{y: "2004 Uniós 50Ft", m: "1 000 000"},
+		{y: "2004 Uniós 50Ft", m: "1 000 000"},
+		{y: "2004 Uniós 50Ft", m: "1 000 000"},
+		{y: "2004 Uniós 50Ft", m: "1 000 000"},
+		{y: "2004 Uniós 50Ft", m: "1 000 000"},
+                {y: "2020 Tisztelet a hősöknek 10Ft", m: "1.997.500"},
+		{y: "2020 Tisztelet a hősöknek 20Ft", m: "1.997.500"},
+		{y: "2021 F betű 5Ft", m: "2.000.000"},
+		{y: "2021 O betű 5Ft", m: "2.000.000"},
+		{y: "2021 R betű 5Ft", m: "2.000.000"},
+		{y: "2021 I betű 5Ft", m: "2.000.000"},
+		{y: "2021 N betű 5Ft", m: "2.000.000"},
+		{y: "2021 T betű 5Ft", m: "2.000.000"},
+		{y: "2004 Uniós 50Ft", m: "1 000 000"},
+
+            ] 
+        }
+    ];
